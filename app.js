c7eba5470823f92301d7471b4f1634ba2bb521aa
Simeon Neisler <simeon.neisler@gmail.com>
2017-08-31 16:30:04 -0400

    adjusted port for deployment

--- app.js
+++ app.js
@@ -51,6 +51,6 @@ app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
 
-app.listen(3000, process.env.IP, (req, res) => {
+app.listen(process.env.PORT, process.env.IP, (req, res) => {
   console.log("Server online");
 });
